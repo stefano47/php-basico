116fce48be1b475785648d66d16b6fc392787fb5
stefanopaulo <stefanopaulo47@gmail.com>
2019-10-13 21:53:12 -0300

    configurando ambiente php

---
+++ olamundo/ola.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <title>Olá - mundo</title>
+
+    <style>
+        h2{
+            color: #80a2ff;
+            text-shadow: 1px 1px 1px #000;
+        }
+    </style>
+</head>
+<body>
+    <h1>Testando PHP</h1>
+
+    <?php 
+        echo "<h2>Olá, mundo!</h2>";
+    ?>
+</body>
+</html>
