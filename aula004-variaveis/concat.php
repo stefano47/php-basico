a1b95ab946d0b102eb7bdb38d5f228b38619a8b6
stefanopaulo <stefanopaulo47@gmail.com>
2019-10-14 12:50:19 -0300

    variaveis em php

---
+++ aula004-variaveis/concat.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <title>Concatenação</title>
+</head>
+<body>
+    <?php
+        $nome = "Stefano";
+        $idade = 23;
+        // echo $nome." tem ".$idade." anos."
+        echo "$nome tem $idade anos." 
+    ?>
+</body>
+</html>
---
+++ aula004-variaveis/tipos.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <title>Tpois de dados</title>
+</head>
+<body>
+    <?php 
+        $n = 4;
+        $no = "Stefano";
+        echo $no;
+    ?>
+</body>
+</html>
